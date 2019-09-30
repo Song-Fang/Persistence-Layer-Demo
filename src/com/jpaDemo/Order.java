9e9a39cda4981067c09b2c20f3a2465609a0a539
hitfangsong <sof10@pitt.edu>
2019-09-29 23:43:45 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

---
+++ src/com/jpaDemo/Order.java
@@ -0,0 +1,46 @@
+package com.jpaDemo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name="JPA_Order")
+@Entity
+public class Order {
+	private int id;
+	private String orderName;
+	private Customer customer;
+
+	@GeneratedValue
+	@Id
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name="ORDER_NAME")
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	@JoinColumn(name = "CUSTOMER_ID")
+	@ManyToOne
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+}
