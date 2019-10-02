85bc41d3ab8f617007d6802804eed72d936c1274
hitfangsong <sof10@pitt.edu>
2019-10-02 10:30:36 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

--- src/com/jpaDemo/Department.java
+++ src/com/jpaDemo/Department.java
@@ -1,5 +1,43 @@
 package com.jpaDemo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Table(name="JPA_Department")
+@Entity
 public class Department {
+	private int ID;
+	private String departmentName;
+	private Manager manager;
+	
+	@JoinColumn(name="Manager_ID")
+	@OneToOne
+	public Manager getManager() {
+		return manager;
+	}
+	public void setManager(Manager manager) {
+		this.manager = manager;
+	}
+	@GeneratedValue
+	@Id
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	
+	@Column(name="Department_Name")
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
 
 }
--- src/com/jpaDemo/Manager.java
+++ src/com/jpaDemo/Manager.java
@@ -3,6 +3,7 @@ package com.jpaDemo;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Table(name="JPA_Manager")
@@ -11,7 +12,16 @@ public class Manager {
 	private int ID;
 	private String managerName;
 	private int age;
+	private Department department;
+
 	
+	@OneToOne(mappedBy="manager")
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 	@GeneratedValue
 	@Id
 	public int getID() {
